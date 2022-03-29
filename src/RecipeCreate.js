5a8f6455df4b2ba2917c3f7d2358c41add57439a
Gabriel <wootengabriel90@gmail.com>
2022-03-29 03:13:03 -0400

    progress on createRecipe form

--- src/RecipeCreate.js
+++ src/RecipeCreate.js
@@ -1,17 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 
-function RecipeCreate() {
-
+function RecipeCreate({ recipes, setRecipes }) {
   // TODO: When the form is submitted, a new recipe should be created, and the form contents cleared.
   // TODO: Add the required input and textarea form elements.
   // TODO: Add the required submit and change handlers
-  
+  const initialFormState = {
+    name: "",
+    cuisine: "",
+    photo: "",
+    ingredients: "",
+    preparation: "",
+  };
+
+  const [formData, setFormData] = useState({ ...initialFormState });
+
+  const handleChange = ({ target }) => {
+    const value = target.value;
+    setFormData({
+      ...formData,
+      [target.name]: value,
+    });
+  };
+
+  const createRecipe = (event) => {
+    event.preventDefault();
+    setRecipes(() => [...recipes, formData]);
+    console.log(recipes);
+  };
+
   return (
-    <form name="create">
+    <form name="create" onSubmit={createRecipe}>
       <table>
         <tbody>
           <tr>
-            <td></td>
+            <td>
+              <label htmlFor="name"></label>
+              <input
+                id="name"
+                name="name"
+                onChange={handleChange}
+                type="text"
+                required={true}
+                value={formData.name}
+                placeholder="Name"
+              />
+            </td>
+            <td>
+              <label htmlFor="cuisine"></label>
+              <input
+                id="cuisine"
+                name="cuisine"
+                onChange={handleChange}
+                type="text"
+                required={true}
+                value={formData.cuisine}
+                placeholder="Cuisine"
+              />
+            </td>
+            <td>
+              <label htmlFor="photo"></label>
+              <input
+                id="photo"
+                name="photo"
+                onChange={handleChange}
+                type="url"
+                required={true}
+                value={formData.photo}
+                placeholder="URL"
+              />
+            </td>
+            <td>4</td>
+            <td>5</td>
             <td>
               <button type="submit">Create</button>
             </td>
