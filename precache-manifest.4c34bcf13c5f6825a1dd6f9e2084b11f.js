f046b4fd60e7c580deec65a136f2f8382fe7f7ac
Gabriel <wootengabriel90@gmail.com>
2022-03-29 22:36:28 -0400

    Updates

---
+++ precache-manifest.4c34bcf13c5f6825a1dd6f9e2084b11f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c85cd3d291169660c0f394e8706a2827",
+    "url": "/recipe-tracking-app-project/index.html"
+  },
+  {
+    "revision": "5061a4f7a5f7a8619d75",
+    "url": "/recipe-tracking-app-project/static/css/main.1bd2e4f2.chunk.css"
+  },
+  {
+    "revision": "f394498dd0083a989c2b",
+    "url": "/recipe-tracking-app-project/static/js/2.6d741cec.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/recipe-tracking-app-project/static/js/2.6d741cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5061a4f7a5f7a8619d75",
+    "url": "/recipe-tracking-app-project/static/js/main.22f7fc35.chunk.js"
+  },
+  {
+    "revision": "4c7e73f2c7cfae38b9b8",
+    "url": "/recipe-tracking-app-project/static/js/runtime-main.fa25c270.js"
+  }
+]);
